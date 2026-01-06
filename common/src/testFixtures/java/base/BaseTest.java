a24ebbc0d7076b9d83754eeb236511003545d08e
margomez <margomez@starbucks.com>
2026-01-06 15:31:55 -0500

    implementing common logger slf4j

--- common/src/testFixtures/java/base/BaseTest.java
+++ common/src/testFixtures/java/base/BaseTest.java
@@ -1,17 +1,24 @@
 package base;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+
 public abstract class BaseTest {
 
+    protected final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @BeforeMethod(alwaysRun = true)
-    public void setUp() {
+    protected void setUp() {
+        log.info("----Test setup started");
         // Setup común (logs, configs, env)
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown(){
+    protected void tearDown(){
+        log.info("----Test teardown finished");
         //cleanup common
     }
 
